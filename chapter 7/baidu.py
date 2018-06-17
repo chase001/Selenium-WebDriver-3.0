b10e173aeac11c91a7544fcc23e6423636a6009f
chase001 <chase001@qq.com>
2018-06-17 13:11:00 +0800

    20180617

---
+++ chapter 7/baidu.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+# driver = selenium.webdriver.Firefox()
+# 
+
+driver.get("https://www.baidu.com")
--- pybase/class_def.py
+++ pybase/class_def.py
@@ -17,4 +17,10 @@ class ClassName(object):
 	def __init__(self, arg):
 		super(ClassName, self).__init__()
 		self.arg = arg
-		
+
+class ClassName(object):
+			"""docstring for ClassName"""
+			def __init__(self, arg):
+				super(ClassName, self).__init__()
+				self.arg = arg
+						
